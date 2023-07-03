b17d2f724c6b981760131725b7601cbea3a97462
lauferg <gergely.laufer@gmail.com>
2023-07-03 11:17:03 +0200

    Initialized repository

---
+++ src/test/java/de/bredex/backendtest/usedcar/UsedcarApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bredex.backendtest.usedcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
